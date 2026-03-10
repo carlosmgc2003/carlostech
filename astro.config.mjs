e279066f291a6702dd1242c32d909f6900aa4fa8
Carlos Alberto Maceira Garcia Coni <carlos@ini.live>
2026-03-10 17:07:23 -0300

    feat: Add Open Graph and Twitter card image support with a new default image and site configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://carlostech.net.ar',
   output: 'server',
   adapter: vercel(),
   vite: {
